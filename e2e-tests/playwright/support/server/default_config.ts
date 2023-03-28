3e35da4a212ae79b94af6bed7bfbd524cf4c7307
Pablo Andrés Vélez Vidal <pablovv2012@gmail.com>
2023-03-28 18:05:19 +0200

    restore the preparing workspace plugins and invite screens

--- e2e-tests/playwright/support/server/default_config.ts
+++ e2e-tests/playwright/support/server/default_config.ts
@@ -73,8 +73,8 @@ const onPremServerConfig = (): Partial<TestAdminConfig> => {
     };
 };
 
-// Should be based only from the generated default config from ./server via "make config-reset"
-// Based on v7.10 server
+// Should be based only from the generated default config from mattermost-server via "make config-reset"
+// Based on v7.9 server
 const defaultServerConfig: AdminConfig = {
     ServiceSettings: {
         SiteURL: '',
@@ -633,7 +633,7 @@ const defaultServerConfig: AdminConfig = {
                 Enable: true,
             },
             focalboard: {
-                Enable: true,
+                Enable: false,
             },
             playbooks: {
                 Enable: true,
@@ -682,16 +682,16 @@ const defaultServerConfig: AdminConfig = {
         BoardsDataRetention: false,
         NormalizeLdapDNs: false,
         EnableInactivityCheckJob: true,
-        UseCaseOnboarding: true,
         GraphQL: false,
         InsightsEnabled: true,
         CommandPalette: false,
-        BoardsProduct: false,
+        BoardsProduct: true,
         SendWelcomePost: true,
         WorkTemplate: false,
         PostPriority: true,
         WysiwygEditor: false,
         PeopleProduct: false,
+        AnnualSubscription: false,
         ReduceOnBoardingTaskList: false,
         OnboardingAutoShowLinkedBoard: false,
         ThreadsEverywhere: false,
